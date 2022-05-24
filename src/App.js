02990a18ac06acea2befd3826e912c6bc90e7815
ShafinRME <shafin.rmedu4@gmail.com>
2022-05-24 17:31:38 +0600

    Route and components is done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import Navbar from './Pages/Shared/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import MyPortfolio from './Pages/MyPortfolio/MyPortfolio';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Blogs from './Pages/Blogs/Blogs';
+import Login from './Pages/Authentication/Login';
+import PageNotFound from './Pages/Shared/PageNotFound';
 
 function App() {
   return (
@@ -11,6 +16,11 @@ function App() {
       <Routes>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/portfolio' element={<MyPortfolio></MyPortfolio>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
       </Routes>
     </div>
   );
