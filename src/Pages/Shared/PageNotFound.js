611dc88a7a04dec3aefd7f7957a1f6c5c7a5234a
ShafinRME <shafin.rmedu4@gmail.com>
2022-05-24 21:47:54 +0600

    NotFound page is done

--- src/Pages/Shared/PageNotFound.js
+++ src/Pages/Shared/PageNotFound.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './PageNotFound.css'
 
 const PageNotFound = () => {
+    // This page not found part
+    const navigate = useNavigate();
+    const handleNavigate = () => {
+        navigate('/home');
+    }
     return (
         <div>
-            <h2>Nothing bro</h2>
+            <div>
+                <div className='footer1'>
+                    <h1>OOOOOPPPPPSSSSSSSS!!!!!!!!</h1>
+                    <p>Page Not Found</p>
+                    <h3>Sorry Dude</h3>
+
+                </div>
+
+                <img className='image' src="https://i.ibb.co/C5ZWpPk/side-image.png" alt="" />
+            </div>
+            <button onClick={handleNavigate} className='footer-button'>Back to Home</button>
+
         </div>
     );
 };
