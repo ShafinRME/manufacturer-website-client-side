51d1cedf20a9290f3e0f1590059971baf849cfa4
ShafinRME <shafin.rmedu4@gmail.com>
2022-05-26 00:54:32 +0600

    Sign in and Sign Out is done

--- src/Pages/Authentication/Login.js
+++ src/Pages/Authentication/Login.js
@@ -38,6 +38,7 @@ const Login = () => {
     const onSubmit = data => {
         console.log(data);
         signInWithEmailAndPassword(data.email, data.password);
+        navigate('/home');
     }
 
     return (
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from 'firebase/auth';
 
 const Navbar = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    const logout = () => {
+        signOut(auth);
+        // localStorage.removeItem('accessToken');
+    }
+
+
     const menuItems = <>
         <li><Link to="/home">Home</Link></li>
         <li><Link to="/portfolio">My Portfolio</Link></li>
         <li><Link to="/blogs">Blogs</Link></li>
         <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/login">Login</Link></li>
+        <li>{user ? <button onClick={logout} className="btn btn-ghost">Sign Out</button> : <Link to="/login">Login</Link>}</li>
     </>
     return (
         <div class="navbar bg-base-100">
