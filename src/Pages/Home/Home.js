67b462a27393ba881d86295896b04d3a500e5f93
ShafinRME <shafin.rmedu4@gmail.com>
2022-05-25 15:06:23 +0600

    Summary is done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import Reviews from './Reviews';
+import Summary from './Summary';
 import Tools from './Tools';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Tools></Tools>
             <Reviews></Reviews>
+            <Summary></Summary>
         </div>
     );
 };
